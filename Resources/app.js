092e71f2d30810a1ffad0722b266205ddfddebc2
Janak Panthee <janakpanthi78@gmail.com>
2024-09-18 13:59:47 +0545

    .

--- Resources/app.js
+++ Resources/app.js
@@ -6,35 +6,42 @@ let dots = document.querySelectorAll('.slider .dots li');
 
 let lengthItems = items.length - 1;
 let active = 0;
-next.onclick = function(){
-    active = active + 1 <= lengthItems ? active + 1 : 0;
+
+next.onclick = function() {
+    active = (active + 1 <= lengthItems) ? active + 1 : 0;
     reloadSlider();
 }
-prev.onclick = function(){
-    active = active - 1 >= 0 ? active - 1 : lengthItems;
+
+prev.onclick = function() {
+    active = (active - 1 >= 0) ? active - 1 : lengthItems;
     reloadSlider();
 }
-let refreshInterval = setInterval(()=> {next.click()}, 3000);
-function reloadSlider(){
+
+let refreshInterval = setInterval(() => { next.click() }, 3000);
+
+function reloadSlider() {
     slider.style.left = -items[active].offsetLeft + 'px';
-    // 
+
     let last_active_dot = document.querySelector('.slider .dots li.active');
-    last_active_dot.classList.remove('active');
-    dots[active].classList.add('active');
+    if (last_active_dot) {
+        last_active_dot.classList.remove('active');
+    }
+
+    if (dots[active]) {
+        dots[active].classList.add('active');
+    }
 
     clearInterval(refreshInterval);
-    refreshInterval = setInterval(()=> {next.click()}, 3000);
-
-    
+    refreshInterval = setInterval(() => { next.click() }, 3000);
 }
 
 dots.forEach((li, key) => {
-    li.addEventListener('click', ()=>{
-         active = key;
-         reloadSlider();
-    })
-})
+    li.addEventListener('click', () => {
+        active = key;
+        reloadSlider();
+    });
+});
+
 window.onresize = function(event) {
     reloadSlider();
 };
-
